e8bf0bac257f2a990fbc2448ec1adc2782381099
Gabriel Such <gabrielsuch@gmail.com>
2016-08-30 23:30:00 -0700

    uncomment second jury test

--- tests/jury_test.rb
+++ tests/jury_test.rb
@@ -19,11 +19,11 @@ class TestJury < Minitest::Test
 
   #=====Remember to uncomment one test method at a time.=====
 
-  # def test_jury_starts_as_empty_array
-  #   jury = Jury.new
-  #   assert_empty jury.members
-  # end
-  #
+  def test_jury_starts_as_empty_array
+     jury = Jury.new
+     assert_empty jury.members
+  end
+  
   # def test_cast_votes_returns_hash
   #   assert_kind_of Hash, @jury.cast_votes(@finalists)
   # end
